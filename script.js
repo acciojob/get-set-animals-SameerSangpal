debd63efba6a9849e6f5f21305a7f35297875ca5
Sameer Sangpal <66773934+SameerSangpal@users.noreply.github.com>
2025-07-14 20:43:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //complete this code
-class Animal {
+if (typeof Animal === 'undefined'){
+	class Animal {
 	constructor(species) {
     this._species = species;
 }
@@ -23,7 +24,6 @@ class Cat extends Animal {
     console.log("purr");
   }
 }
-
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
